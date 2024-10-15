00e75a3d4b26bf82acc21f77069bcca46e24eaa3
Shivani Maurya <shivani.shivi333@gmail.com>
2024-10-15 23:41:22 +0530

    photfolio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./header"
+import AddPhoto from "./add-photo"
+
+
+function App() {
+    return (
+        <div>
+            
+            <Header/>
+            <AddPhoto/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <img src ="https://cdn-icons-png.flaticon.com/128/3979/3979360.png  " alt ="photo album" />
+            <h1>Photo Folio</h1>
+        </div>
+    );
+}
+
+export default Header;
