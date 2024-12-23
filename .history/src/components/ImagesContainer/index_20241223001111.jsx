61f92f970452e657f9433a2cecf4c8b28daaaaf1
Shivani Maurya <shivani.shivi333@gmail.com>
2024-12-23 23:25:43 +0530

    update delete btn

---
+++ .history/src/components/ImagesContainer/index_20241223001111.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./index.css";
+
+export function ImagesContainer({ currentAlbumName, albumImages }) {
+  return (
+    <>
+      <div className="images">
+        {(albumImages[currentAlbumName] || []).map((titleImg, index) => (
+          <div key={index} className="image-container">
+            <i
+            <div>
+              <img alt={`Image of ${titleImg.title}`} className="image" />
+              <span>{titleImg.title}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
